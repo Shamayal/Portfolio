eb178e88d42c9089d545ebc93db8d37533c0de09
Shamayal Syed <shamayalsyed@Shamayals-MacBook-Pro.local>
2022-09-09 00:18:25 -0400

    Update website navbar, header and body with stylings and images.

---
+++ script.js
@@ -0,0 +1,18 @@
+const header = document.querySelector("nav");
+
+window.onscroll = function () {
+    const top = window.scrollY;
+
+    if (top >= 100) {
+        header.classList.add("navbarPink");
+    } else {
+        header.classList.remove("navbarPink");
+    }
+};
+
+var typed = new Typed('#typed', {
+    strings: ['UofT 2021 HBSc Graduate', 'Intern @ Zurich Canada', 'Beginner in Web Development', 'Social Media Content Creator', 'Dementia Research Assistant'],
+    typeSpeed: 30,
+    loop: true,
+    backSpeed: 50
+  })
